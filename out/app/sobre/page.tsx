b47f19cdbe986727cfd7bca599e403e5066c4bad
GabrielGCSOARES <gs519636@gmail.com>
2026-04-27 15:14:40 -0300

    small patch

--- out/app/sobre/page.tsx
+++ out/app/sobre/page.tsx
@@ -8,7 +8,7 @@ export default function Sobre() {
       <main className="p-6 flex flex-col items-center">
         <h1 className="text-2x1 font-bold mb-4">Quem sou eu</h1>
         <div>
-          <img src="https://i.pinimg.com/736x/ca/8b/49/ca8b49812e52e0139844e5837a6a4b23.jpg" alt="" className="rounded-lg h-100 "/>
+          <img src="https://i.pinimg.com/736x/25/9e/06/259e06edd79559d0dd1fce29d4772817.jpg" alt="" className="rounded-lg h-100 transition-transform duration-500 hover:scale-105 "/>
         </div>
         <p className="text-center text-gray-600 mt-4 max-w-md">Sou um desenvolvedor full-stack com experiência em React, React Native e tecnologias relacionadas. Gosto de criar aplicações webs e bater em crianças no jogo online fazendo-as dar ragequit e me ameaçar de morte</p>
       </main>
